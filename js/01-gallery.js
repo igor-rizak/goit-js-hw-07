5fce0f4c4e2912d93cbdc84df8f4d91e044efee7
igor-rizak <igor.rizak@ukr.net>
2023-04-23 08:07:07 +0300

    js 07hw_01

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,6 +1,38 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryList = document.querySelector('.gallery');
 
- console.log(message)
+
+const markupGallery = galleryItems.map((galleryItem) => `<li class="gallery__item">
+  <a class="gallery__link" href="large-image.jpg">
+    <img
+      class="gallery__image"
+      src="${galleryItem.preview}"
+      data-source="${galleryItem.original}"
+      alt="${galleryItem.description}"
+    />
+  </a>
+</li>`).join('');
+
+
+galleryList.insertAdjacentHTML("afterbegin", markupGallery);
+
+
+const galeryList = document.addEventListener('click', onTargetClickGalllery);
+
+function onTargetClickGalllery(event) {
+    event.preventDefault();
+    if (event.target.nodeName !== 'IMG') {
+        return;
+    }
+    const linkLargeImage = event.target.dataset.source;
+
+    const instance = basicLightbox.create(`
+    <img src="${linkLargeImage}" width="800" height="600">
+    `)
+
+    instance.show()
+}
+
+console.log(galleryItems);
