f1e9af3df109b5d1276086949ab326c813e39ac1
igor-rizak <igor.rizak@ukr.net>
2023-04-23 10:20:41 +0300

    comit

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -3,25 +3,25 @@ import { galleryItems } from './gallery-items.js';
 
 const galleryList = document.querySelector('.gallery');
 
-const markupGallery = galleryItems.map((galleryItem) => `<li class="gallery__item">
+const markupGallery = galleryItems.map(({preview, original, description}) => `<li class="gallery__item">
   <a class="gallery__link" href="large-image.jpg">
     <img
       class="gallery__image"
-      src="${galleryItem.preview}"
-      data-source="${galleryItem.original}"
-      alt="${galleryItem.description}"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
     />
   </a>
 </li>`).join('');
 
 galleryList.insertAdjacentHTML("afterbegin", markupGallery);
 
-const galeryList = document.addEventListener('click', onTargetClickGalllery);
+galleryList.addEventListener('click', onTargetClickGalllery);
 
 function onTargetClickGalllery(event) {
-    event.preventDefault();
-    if (event.target.nodeName !== 'IMG') {
-        return;
+  event.preventDefault();
+  if (event.target.nodeName !== 'IMG') {
+      return;
     }
     const linkLargeImage = event.target.dataset.source;
 
